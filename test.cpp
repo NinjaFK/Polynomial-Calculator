971cfc3cd2e17f4bbd5de080a2a6bff69d039d52
Brian <nieh_brian@yahoo.com>
2022-11-09 23:52:21 -0800

    test comments

--- test.cpp
+++ test.cpp
@@ -11,6 +11,18 @@ int main()
     polynomial result;
 
     result = poly1 * poly2;
+
+    /* std::cout << "i: " << i << '\n';
+            std::cout << "j: " << j << '\n';
+            std::cout << "This poly " << this->polyExpr[i] << '\n';
+            std::cout << "Rhs poly " << rhs.polyExpr[j] << '\n';
+            std::cout << "Result :";
+            for (int l = 0; l < result.degree + 1; l++)
+            {
+                std::cout << '[' << result.polyExpr[l] << "] ";
+            }
+            std::cout << '\n'
+                      << '\n'; */
     std::cout << "result = " << result << "\n";
 
     return 0;
